e6e2c177bc0dcb6898b34f5b9979a4c8e37aa918
damian-zawadzki <dam.zawadzki@gmail.com>
2022-10-28 12:47:26 +0200

    mvp11

---
+++ staticfiles/functions.a9c64b5f166b.js
@@ -0,0 +1,8 @@
+function showHide() {
+  var file = document.getElementById("file_upload");
+  if (file.style.display === "none") {
+    file.style.display = "block";
+  } else {
+    file.style.display = "none";
+  }
+}
