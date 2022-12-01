5282f87cd75892ba88b8821fbacfd1cb3ef496ff
damian-zawadzki <dam.zawadzki@gmail.com>
2022-12-01 11:48:57 +0100

    homework14

--- nieszkolni_app/views.py
+++ nieszkolni_app/views.py
@@ -4831,6 +4831,8 @@ def timesheet(request):
         last_name = request.user.last_name
         current_user = first_name + " " + last_name
 
+        AuditManager().remove()
+
         if request.method == "POST":
             if request.POST["action_on_timesheet"] == "filter":
                 start = request.POST["start"]
