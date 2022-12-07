62ec176e19fd3a16240aa9d430e1fdfe8339bc77
damian-zawadzki <dam.zawadzki@gmail.com>
2022-12-07 09:53:57 +0100

    timesheet for superuser 3

--- nieszkolni_app/views.py
+++ nieszkolni_app/views.py
@@ -378,15 +378,14 @@ def register_user(request):
             username = first_name_variable + " " + last_name_variable
 
             user = authenticate(request, username=system_username, password=password)
+            add = ClientsManager().add_client(
+                username,
+                internal_email_address
+                )
             if user is None:
                 user = User.objects.create_user(system_username, internal_email_address, password)
                 user.first_name = first_name_variable
                 user.last_name = last_name_variable
-
-                add = ClientsManager().add_client(
-                    username,
-                    internal_email_address
-                    )
 
                 is_client = ClientsManager().verify_client(username)
                 if is_client is False:
